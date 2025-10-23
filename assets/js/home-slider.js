4136f1c6408d10f91d84c799fee4e115b5225fc3
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-10-23 18:30:34 +0300

    added schedule section

---
+++ assets/js/home-slider.js
@@ -0,0 +1,16 @@
+(() => {
+  // initialize swiper
+  new Swiper('#schedule-slider', {
+    // defines pagination container
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // additional settings
+    slidesPerView: 'auto',
+    // specifies gap
+    spaceBetween: 4,
+    // allow grabbing
+    grabCursor: true,
+  })
+})()
